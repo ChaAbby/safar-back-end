765c635a8a3c91d204c03b294f6611043e7c9a4a
Abigail Charime <abigail.charime@gmail.com>
2021-08-16 01:09:41 -0700

    corrected code

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,8 +11,8 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/safar_development'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI")
     db.init_app(app)
     migrate.init_app(app, db)
 
